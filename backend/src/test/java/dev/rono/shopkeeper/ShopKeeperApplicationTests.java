6a76e43562d758c04bae6b89f2d2600f79eca0b2
Brian Rono C. K. <bkcrono@gmail.com>
2025-07-08 20:51:52 +0200

    Initial Spring Boot backend with Folder Structure

---
+++ backend/src/test/java/dev/rono/shopkeeper/ShopKeeperApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.rono.shopkeeper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopKeeperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
